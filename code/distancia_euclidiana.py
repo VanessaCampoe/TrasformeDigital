a4e0817a8d4a69ed469d06d63d1692dcd1014615
VanessaCampoe <vanessacampoe@gmail.com>
2026-05-05 21:38:28 -0300

    tec commit

---
+++ code/distancia_euclidiana.py
@@ -0,0 +1,44 @@
+# O que é classe :
+# Um conjunto de objetos 
+
+class DistanciaEuclidiana:
+    ''' atributos e construtores
+    
+    '''
+    
+    #Joaquim chegou a feira de goiania e quer comprar roupas o ponto de localização  do joaquim é (2,3) e o ponto é a distância entre os dois pontos??
+    # construtor padrão ( se o programador não cria um construtor, o python cria um construtor padrao )
+    # O cosntrutor padrão é automatico
+    #O construtor com parametrod e criado pelo programador 
+    def __init__(self, x1, y1):
+        self.x1 = x1
+        self.y1 = y1
+    
+    
+    
+    """metodos especiais ( o construtor tb e um metodo especial __init__) mais deixa isso para depois 
+    """
+    def __str__(self):
+        return f"(ponto1={self.x1},ponto2={self.y2})"
+""" Métodos Workes"""
+def calcular_distancia(self, x1, y2):
+    
+    # passo 1 - diferença vertical - eixo x
+    
+    
+    # passo 2 - diferença vertical - eixo y
+    
+    
+    # passo 3 - teorema de pitagoras 
+    return 0
+    
+    
+    def calcular_distancia_completa(self, x1, y2):
+        
+    # passo 1 - diferença vertical
+    
+    
+    # passo 2 - diferença vertical 
+    
+    
+    # passo 3 - teorema de pitagoras 
---
+++ code/executor_de_objetos.py
@@ -0,0 +1,5 @@
+
+from distancia_euclidiana import DistanciaEuclidiana 
+
+ponto_inicial = DistanciaEuclidina(2,3)
+print(ponto_inicial)
